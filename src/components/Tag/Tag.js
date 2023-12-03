19b5044d84ed22c21c7cc8feea919e955f51bcde
go <xxxviii00@gmail.com>
2023-12-03 21:28:28 +0900

    fix: 태그 클릭이벤트 navigate -> Link

--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -1,20 +1,16 @@
-import { navigate } from "gatsby"
+import { Link, navigate } from "gatsby"
 import React from "react"
 const TagBox = ({ children }) => {
   return <div className="flex gap-1.5 my-3">{children}</div>
 }
 
 const Tag = ({ text }) => {
-  const onClick = () => {
-    navigate(`/tags/${text}`)
-  }
   return (
-    <div
-      className="text-text-default px-4 font-normal text-opacity-80 bg-background-primary bg-opacity-30 border-text-default border border-opacity-20 rounded-2xl text-xs  leading-6 hover:bg-accent-default hover:bg-opacity-50 hover:border-accent-default cursor-pointer transition "
-      onClick={onClick}
-    >
-      {text}
-    </div>
+    <Link to={`/tags/${text}`}>
+      <div className="text-text-default px-4 font-normal text-opacity-80 bg-background-primary bg-opacity-30 border-text-default border border-opacity-20 rounded-2xl text-xs  leading-6 hover:bg-accent-default hover:bg-opacity-50 hover:border-accent-default cursor-pointer transition ">
+        {text}
+      </div>
+    </Link>
   )
 }
 
