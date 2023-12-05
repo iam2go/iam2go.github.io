b936b32dc68f2225ca16dbeffdf6c8383813d2dd
go <xxxviii00@gmail.com>
2023-12-05 22:28:38 +0900

    feat: 404 페이지 추가

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,14 +3,26 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
     <Layout location={location} title={siteTitle}>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className="my-32 text-text-default text-center">
+        <StaticImage
+          className="my-8 mx-auto"
+          layout="fixed"
+          formats={["auto", "webp", "avif"]}
+          src="../images/404.png"
+          width={300}
+          quality={95}
+          alt="Profile picture"
+        />
+        <div>페이지를 찾을 수 없어요.</div>
+        <div> 페이지의 주소를 다시 한번 확인 해 주세요!</div>
+      </div>
     </Layout>
   )
 }
