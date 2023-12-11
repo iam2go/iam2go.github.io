dc2c49f2341b577be4c60a0144361170bf1e472e
go <xxxviii00@gmail.com>
2023-12-11 23:53:22 +0900

    style: Favicon 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,9 +16,6 @@ module.exports = {
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
   },
   plugins: [
     `gatsby-plugin-postcss`,
@@ -144,15 +141,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `leego.dev.log`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/leego_favicon.png`, // This path is relative to the root of the site.
       },
     },
   ],
